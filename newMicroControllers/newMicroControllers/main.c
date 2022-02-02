f159051a2fa398e1142e2ca243b1fb1766c5ae52
Redouan <redouan778@gmail.com>
2022-02-02 21:41:59 +0100

    Opdracht B-4

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -14,19 +14,18 @@ int main( void ) {
 	DDRD = 0b11111111; // Alle pins van DDRD worden als output gezet.
 	
 	while (1) {
-		//If-statement checkt of drukknop op PINC-1 is ingedrukt
-		//zoja, dan gaat het ledje knipperen
-		//zoniet, dan blijft het ledje aan zonder enige instructie.
-		if(PINC == 0b00000001){ 
-			PORTD = 0b10000000;
-			wait(500);
-			PORTD = 0b00000000;
-			wait(500);
-		}else{
-			PORTD = 0b10000000;
+		int i = 0;
+		
+		while (1){
+			PORTD = (1 << i);
+			i++;
+			
+			if (i > 7){
+				i = 0;
+			}
+			wait(50);
 		}
-	}
-	
+	}	
 	
 	return 1;
 }
