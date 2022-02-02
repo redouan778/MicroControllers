5dd7a92f77a6b235cacfa18c35e2f3096ffda811
Redouan <redouan778@gmail.com>
2022-02-02 21:53:12 +0100

    Opdracht B-6

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -12,23 +12,23 @@ void wait( int ms ){
 
 int main( void ) {
 	DDRD = 0b11111111; // Alle pins van DDRD worden als output gezet.
+	int speed = 500; //knipper snelheid als variable opgeslagen.
 	
-	//array dat alle pins bevat inclucsief adres.
-	int ledIds[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+	while (1) {
+		PORTD = 0b10000000; //Led gaat aan voor een half seconde.
+		wait(speed);  
+		PORTD = 0b00000000; // led gaat uit voor een half seconde.
+		wait(speed);
 	
-	//counter dat bij houdt welke index het is van de array hierboven
-	int i = 0;
-	
-	while (1){
-		PORTD = ledIds[i];
-	
-		i++;
-	
-		//checkt of de counter niet boven 7 komt om een indexoutofbounds te voorkomen. Dan wordt de counter weer op 0 gezet.
-		if (i > 7){
-			i = 0;
-		}	
-		wait(50);
+		// check of de juiste knop wordt ingedrukt om te bepalen wat de snelheid is.
+		if (PINC == 0b00000001) {
+			//Deze if-statement checkt op welke snelheid het lampje knippert en pas het zodanig aan.
+			if(speed == 500) {
+				speed = 125;
+			} else {
+				speed = 500;
+			}
+		}
 	}
 	
 	return 1;
