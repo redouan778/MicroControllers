649e9a0bf2be1ef8582563d95066cbfd700e03a9
Redouan <redouan778@gmail.com>
2022-02-02 21:43:56 +0100

    Opdracht B-5

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -13,19 +13,23 @@ void wait( int ms ){
 int main( void ) {
 	DDRD = 0b11111111; // Alle pins van DDRD worden als output gezet.
 	
-	while (1) {
-		int i = 0;
-		
-		while (1){
-			PORTD = (1 << i);
-			i++;
-			
-			if (i > 7){
-				i = 0;
-			}
-			wait(50);
-		}
-	}	
+	//array dat alle pins bevat inclucsief adres.
+	int ledIds[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+	
+	//counter dat bij houdt welke index het is van de array hierboven
+	int i = 0;
+	
+	while (1){
+		PORTD = ledIds[i];
+	
+		i++;
+	
+		//checkt of de counter niet boven 7 komt om een indexoutofbounds te voorkomen. Dan wordt de counter weer op 0 gezet.
+		if (i > 7){
+			i = 0;
+		}	
+		wait(50);
+	}
 	
 	return 1;
 }
