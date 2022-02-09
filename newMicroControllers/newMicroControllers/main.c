2c7fb829524343a919aa9304bcd02409c34341b2
Redouan <redouan778@gmail.com>
2022-02-09 16:49:56 +0100

    Opdracht B-3

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -9,45 +9,70 @@ void wait( int ms ) {
 	}
 }
 
-int ledIndex = 0; //Current selected led
+const char Numbers [15] = {
+	0b00111111,		// 0
+	0b00000110,		// 1
+	0b01011011,		// 2
+	0b01001111,		// 3
+	0b01100110,		// 4
+	0b01101101,		// 5
+	0b01111101,		// 6
+	0b00000111,		// 7
+	0b01111111,		// 8
+	0b01101111,		// 9
+	0b01110111,     //A
+	0b01111100,		//B
+	0b00111001,		//C
+	0b01011110,		//D
+	0b01111001,		//E
+	0b01110001		//F
+};
 
-ISR( INT1_vect ) {	
-	//next led with a check
-	ledIndex++; 		
-	if (ledIndex > 7){
-		ledIndex = 0;
-	}
-	
-    PORTB = (1<<ledIndex);	
+int chosenNumber;
+
+//Checks the number and dispalys it to the 7-segment display
+void display(int digit){
+	if(digit >= 0 && digit <= 15){						
+		PORTD = Numbers[digit];							
+	} else{
+		PORTD = Numbers[14];
+	}	
 }
 
-
-ISR( INT2_vect ) {
-	//Previous led with a check
-	ledIndex--;	
-	if (ledIndex < 0) {
-		ledIndex = 7;
-	}
-
-    PORTB = (1<<ledIndex);		
+//Substracts the number with one down
+void digitDOWN(){		
+	display(chosenNumber--);
 }
 
+//Adds one digit to the number
+void digitUP(){	
+	display(chosenNumber++);
+} 
 
 
 int main( void ) {
 	// Init I/O
-	DDRD = 0x00;			//These are for the input buttons
-	DDRB = 0xFF;			//These are the output leds
-	PORTB = 0x01;			//Start led
+	DDRD = 0xFF;			//These are for the input buttons
+	PORTD = 0x00;		
+	PORTB = 0x01;
 
-	// Init Interrupt hardware
-	EICRA |= 0b00111100;			
-	EIMSK |= 0b00000110;			
-	
 	sei();				
 
+	chosenNumber = 4;
+	
 	while (1) {		
-		wait( 500 );								
+		wait( 200 );	
+		display(chosenNumber);	
+		
+		//checks if the first button is pressed
+		if (PINB == 0b00000001) {
+			digitUP();
+		}
+		
+		//checks if the second button is pressed
+		if (PINB == 0b00000010) {
+			digitDOWN();
+		}								
 	}
 
 	return 1;
