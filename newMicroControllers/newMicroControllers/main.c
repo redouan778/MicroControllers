0d4ed886432d382c6bad1baec9d98014b302d34a
Redouan <redouan778@gmail.com>
2022-02-09 10:41:28 +0100

    Started with week-2

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -12,14 +12,14 @@ void wait( int ms ){
 
 int ledIds[3] = {0b00000001, 0b00000010, 0b00000100};
 	
-
+//methode zorgt ervoor dat er bepaalt wordt door de switch-case wat de volgerde wordt en wat de tri-state wordt.
 void setCharliePlexingLed(int led){
 	switch(led){
 		case 1:
 		DDRD = 0b11111011;
 		PORTD = ledIds[0];
-		
-		break;
+		asdasdas
+		break;sdfsdfs
 		case 2:
 		DDRD = 0b11111011;
 		PORTD = ledIds[1];
@@ -48,21 +48,20 @@ void setCharliePlexingLed(int led){
 	
 int main( void ) {
 	DDRD = 0b11111111; // Alle pins van DDRD worden als output gezet.
-	
-		int i = 1;
+	int i = 1;
 		
-		while(1){
-			
-			setCharliePlexingLed(i);
-			i++;
-			
-			if (i > 6)
-			{
-				i= 1;
-			}
-			
-			wait(500);				
+	while(1){			
+		//De methode met als parameter welke lampje aan de beurt is.
+		setCharliePlexingLed(i);
 		
+		//Na het versturen wordt het met één opgehoogd om de volgende lamp aan te roepen.
+		i++;
+			
+		//Check of de 6e lamp al is aangeroepen zoja, dan begint de teller weer op 1;
+		if (i > 6) {
+			i = 1;
+		}			
+		wait(500);						
 	}
 	
 	return 1;
