d35e11c183e4bd48f2c7d1e6ea4fa193bbb0311e
Redouan <redouan778@gmail.com>
2022-02-23 10:39:31 +0100

    WEEK3 Opdracht B-1

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -4,50 +4,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-typedef struct {
-unsigned char data;
-unsigned int delay ;
-} PATTERN_STRUCT;
-
-PATTERN_STRUCT pattern[] = {
-{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100},
-{0x00, 100},
-{0xAA,  50}, {0x55,  50},
-{0xAA,  50}, {0x55,  50},
-{0xAA,  50}, {0x55,  50},
-{0x00, 100},
-{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, {0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200},
-{0x00, 0x00}
-};
-
-/*Busy wait number of millisecs*/
 void wait( int ms ) {
-for (int i=0; i<ms; i++) {
-_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
-}
-}
-
-
-/*
-main() loop, entry point of executable
-*/
-int main( void )
-{
-DDRD = 0b11111111;					// PORTD all output
-
-while (1==1) {
-// Set index to begin of pattern array
-int index = 0;
-// as long as delay has meaningful content
-while( pattern[index].delay != 0 ) {
-// Write data to PORTD
-PORTD = pattern[index].data;
-// wait
-wait(pattern[index].delay);
-// increment for next round
-index++;
-}
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
 }
 
-return 1;
+
+void init(){
+	DDRD &= 0b01111111;
+	TCCR2 = 0b00000111;
+}
+
+int main(void) {
+	DDRB = 0xFF;
+	init();
+	
+	while (1) {
+		PORTB = TCNT2;
+		wait(10);
+	}
 }
