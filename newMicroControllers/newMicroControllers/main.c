49f9bd6b6b04013560ec701178287b567ce1e3d6
Redouan <redouan778@gmail.com>
2022-02-23 11:21:33 +0100

    WEEK3 Opdracht B-2

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -1,8 +1,8 @@
-
 #define F_CPU 8e6
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 void wait( int ms ) {
 	for (int i=0; i<ms; i++) {
@@ -10,18 +10,39 @@ void wait( int ms ) {
 	}
 }
 
+void timer2Init( void ) {
+	OCR2 = 195;
+	TIMSK |= 0b10000000;
+	sei();
+	TCCR2 = 0b00000101;
+}
+
+
+ISR( TIMER2_OVF_vect ) {
+	if (OCR2 == 195) {
+		PORTD = 0b10000000;
+		OCR2 = 117;
+		}else if(OCR2 == 117){
+		PORTD = 0b00000000;
+		OCR2 = 195;
+	}
+}
+
 
 void init(){
-	DDRD &= 0b01111111;
-	TCCR2 = 0b00000111;
+	DDRD &= 0b11111111;
 }
 
 int main(void) {
 	DDRB = 0xFF;
 	init();
 	
+	timer2Init();
+	PORTD = 0b00000000;
+	
 	while (1) {
-		PORTB = TCNT2;
+		
 		wait(10);
+		
 	}
 }
