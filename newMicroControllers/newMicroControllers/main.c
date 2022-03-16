ae300e003fc85c2c1e1ac8096b1221fad102ea42
Redouan <redouan778@gmail.com>
2022-03-16 11:45:47 +0100

    Opdracht B-3

--- newMicroControllers/newMicroControllers/main.c
+++ newMicroControllers/newMicroControllers/main.c
@@ -5,34 +5,32 @@
 
 #define BIT(x)	(1 << (x))
 
-void wait( int ms )
-{
-	for (int tms=0; tms<ms; tms++)
-	{
+// wait(): busy waiting for 'ms' millisecond
+// Used library: util/delay.h
+void wait( int ms ){
+	for (int tms=0; tms<ms; tms++){
 		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
 	}
 }
 
-//Setup the the admux & adcsrqa
-void adcInit( void )
-{
-	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
-	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+
+// Initialize ADC: 
+void adcInit( void ){
+	ADMUX = 0b11100001;			// AREF=2,56 V, result left adjusted, channel1 at pin PF1
+	ADCSRA = 0b10000110;		// ADC-enable, no interrupt, no free running, division by 64
 }
 
 
-int main( void )
-{
-	DDRF = 0x00;				// set PORTF for input (ADC)
-	DDRA = 0xFF;				// set PORTA for output 
-	DDRB = 0xFF;				// set PORTB for output
-	adcInit();					// initialize ADC
+// Main program: Counting on T1
+int main( void ){
+	DDRF = 0x00;					// set PORTF for input (ADC)
+	DDRA = 0xFF;					// set PORTA for output 
+	adcInit();						// initialize ADC
 
-	while (1)
-	{
-		
-		PORTB = ADCL;			
-		PORTA = ADCH;
-		wait(100);				
+	while (1) {
+		ADCSRA |= BIT(6);				// Start ADC
+		while ( ADCSRA & BIT(6) ) ;		// Wait for completion
+		PORTA = ADCH;					// Show MSB (bit 9:2) of ADC
+		wait(500);						// every 50 ms (busy waiting)
 	}
 }
